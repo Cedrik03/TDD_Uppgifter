555a00d8777db92c55f66d7fc62418b430ac7069
Cedrik03 <cedrik.ressle@stud.sti.se>
2025-04-29 16:42:38 +0200

    Cedrik och Amanda

--- src/main/java/CalculatorService.java
+++ src/main/java/CalculatorService.java
@@ -1,2 +1,24 @@
 public class CalculatorService {
+    public int add(int a, int b) {
+        return a + b;
+    }
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public int divide(int a, int b) {
+        if (b == 0) throw new ArithmeticException("Division by zero");
+        return a / b;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public int add(String a, String b) {
+        return Integer.parseInt(a) + Integer.parseInt(b);
+    }
 }
